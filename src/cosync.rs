24f28a2623f26b9eeef116a6509e2974521e553e
Jonathan Spira <jjspira@gmail.com>
2022-02-06 17:32:59 -0500

    working on doc comments

--- src/cosync.rs
+++ src/cosync.rs
@@ -23,8 +23,8 @@ use super::futures::{enter::enter, FuturesUnordered};
 /// queue tasks within other tasks.
 ///
 /// You can queue a task by using [queue](Cosync::queue), by spawning a [CosyncCoQueue]
-/// and calling [queue](CosyncCoQueue::queue), or, within a task, calling [queue_task](CosyncInput::queue)
-/// on [CosyncInput].
+/// and calling [queue](CosyncCoQueue::queue), or, within a task, calling
+/// [queue_task](CosyncInput::queue) on [CosyncInput].
 pub struct Cosync<T> {
     pool: FuturesUnordered<FutureObject>,
     incoming: Arc<Mutex<VecDeque<IncomingObject>>>,
@@ -33,34 +33,29 @@ pub struct Cosync<T> {
 
 /// Guarded Input.
 ///
-/// Its primary role is to create a [CosyncInputGuard] by [get] and to queue
-/// more tasks by [queue]
+/// Its primary role is to create a [CosyncInputGuard] by [get] and to queue more tasks by [queue]
 ///
-/// #add_task: Self::queue
-/// #get: Self::get
+/// [queue]: Self::queue
+/// [get]: Self::get
 pub struct CosyncInput<T> {
     heap_ptr: *const Option<NonNull<T>>,
     incoming: Arc<Mutex<VecDeque<IncomingObject>>>,
 }
 
 impl<T: 'static> CosyncInput<T> {
-    /// Gets the underlying [CosyncInputGuard]
+    /// Gets the underlying [CosyncInputGuard].
     pub fn get(&mut self) -> CosyncInputGuard<'_, T> {
         // we can always dereference this data, as we maintain
         // that it's always present.
         let box_ref = unsafe { &*self.heap_ptr };
 
         // when we unwrap this, we can also AsRef it
-        let o = unsafe {
-            box_ref
-                .expect("single executor was not initialized this run correctly")
-                .as_mut()
-        };
+        let o = unsafe { box_ref.expect("cosync was not initialized this run correctly").as_mut() };
 
         CosyncInputGuard(o)
     }
 
-    /// Adds a new Task to the TaskQueue.
+    /// Queues a new task. This goes to the back of queue.
     pub fn queue<Task, Out>(&mut self, task: Task)
     where
         Task: Fn(CosyncInput<T>) -> Out + Send + 'static,
@@ -82,22 +77,153 @@ impl<T> fmt::Debug for CosyncInput<T> {
     }
 }
 
-/// A guarded pointer. This exists to prevent holding onto
-/// the `CosyncInputGuard` over `.await` calls. It will need
-/// to be fetched again from [CosyncInput] after awaits.
+/// A guarded pointer.
+///
+/// This exists to prevent holding onto the `CosyncInputGuard` over `.await` calls. It will need to
+/// be fetched again from [CosyncInput] after awaits.
 pub struct CosyncInputGuard<'a, T>(&'a mut T);
 
 impl<'a, T> ops::Deref for CosyncInputGuard<'a, T> {
-    type Target = &'a mut T;
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        self.0
     }
 }
 
 impl<'a, T> ops::DerefMut for CosyncInputGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        self.0
+    }
+}
+
+impl<T: 'static> Cosync<T> {
+    /// Create a new, empty pool of tasks.
+    pub fn new() -> Self {
+        Self {
+            pool: FuturesUnordered::new(),
+            incoming: Default::default(),
+            data: Box::new(None),
+        }
+    }
+
+    /// Adds a new Task to the TaskQueue.
+    pub fn queue<Task, Out>(&mut self, task: Task)
+    where
+        Task: Fn(CosyncInput<T>) -> Out + Send + 'static,
+        Out: Future<Output = ()> + Send,
+    {
+        let position = &*self.data as *const Option<_>;
+
+        queue_task(task, position, &self.incoming);
+    }
+
+    /// Run all tasks in the pool to completion.
+    ///
+    /// ```
+    /// use futures::executor::LocalPool;
+    ///
+    /// let mut pool = LocalPool::new();
+    ///
+    /// // ... spawn some initial tasks using `spawn.spawn()` or `spawn.spawn_local()`
+    ///
+    /// // run *all* tasks in the pool to completion, including any newly-spawned ones.
+    /// pool.run();
+    /// ```
+    ///
+    /// The function will block the calling thread until *all* tasks in the pool
+    /// are complete, including any spawned while running existing tasks.
+    pub fn run(&mut self, parameter: &mut T) {
+        // hoist the T:
+        unsafe {
+            *self.data = Some(NonNull::new_unchecked(parameter as *mut _));
+        }
+
+        run_executor(|cx| self.poll_pool(cx));
+
+        // for segfault help, we null here
+        *self.data = None;
+    }
+
+    /// Runs all tasks in the queue and returns if no more progress can be made
+    /// on any task.
+    ///
+    /// ```
+    /// use futures::{
+    ///     executor::LocalPool,
+    ///     future::{pending, ready},
+    ///     task::LocalSpawnExt,
+    /// };
+    ///
+    /// let mut pool = LocalPool::new();
+    /// let spawner = pool.spawner();
+    ///
+    /// spawner.spawn_local(ready(())).unwrap();
+    /// spawner.spawn_local(ready(())).unwrap();
+    /// spawner.spawn_local(pending()).unwrap();
+    ///
+    /// // Runs the two ready task and returns.
+    /// // The empty task remains in the pool.
+    /// pool.run_until_stalled();
+    /// ```
+    ///
+    /// This function will not block the calling thread and will return the moment
+    /// that there are no tasks left for which progress can be made;
+    /// remaining incomplete tasks in the pool can continue with further use of one
+    /// of the pool's run or poll methods. While the function is running, all tasks
+    /// in the pool will try to make progress.
+    pub fn run_until_stalled(&mut self, parameter: &mut T) {
+        // hoist the T:
+        unsafe {
+            *self.data = Some(NonNull::new_unchecked(parameter as *mut _));
+        }
+
+        poll_executor(|ctx| {
+            let _ = self.poll_pool(ctx);
+        });
+
+        // null it
+        *self.data = None;
+    }
+
+    // Make maximal progress on the entire pool of spawned task, returning `Ready`
+    // if the pool is empty and `Pending` if no further progress can be made.
+    fn poll_pool(&mut self, cx: &mut Context<'_>) -> Poll<()> {
+        // state for the FuturesUnordered, which will never be used
+        loop {
+            let ret = self.poll_pool_once(cx);
+
+            // we queued up some new tasks; add them and poll again
+            // if !self.incoming.is_empty() {
+            //     continue;
+            // }
+
+            // no queued tasks; we may be done
+            match ret {
+                Poll::Pending => return Poll::Pending,
+                Poll::Ready(None) => return Poll::Ready(()),
+                _ => {}
+            }
+        }
+    }
+
+    // Try make minimal progress on the pool of spawned tasks
+    fn poll_pool_once(&mut self, cx: &mut Context<'_>) -> Poll<Option<()>> {
+        // grab our next task...
+        if self.pool.is_empty() {
+            if let Some(task) = self.incoming.lock().unwrap().pop_front() {
+                unsafe { self.pool.push(task.into_future_object()) }
+            }
+        }
+
+        // try to execute the next ready future
+        Pin::new(&mut self.pool).poll_next(cx)
+    }
+}
+
+impl<T: 'static> Default for Cosync<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -229,313 +355,14 @@ fn queue_task<T: 'static, Task, Out>(
     incoming.lock().unwrap().push_back(IncomingObject(our_cb));
 }
 
-impl<T: 'static> Cosync<T> {
-    /// Create a new, empty pool of tasks.
-    pub fn new() -> Self {
-        Self {
-            pool: FuturesUnordered::new(),
-            incoming: Default::default(),
-            data: Box::new(None),
-        }
-    }
-
-    /// Adds a new Task to the TaskQueue.
-    pub fn queue<Task, Out>(&mut self, task: Task)
-    where
-        Task: Fn(CosyncInput<T>) -> Out + Send + 'static,
-        Out: Future<Output = ()> + Send,
-    {
-        let position = &*self.data as *const Option<_>;
-
-        queue_task(task, position, &self.incoming);
-    }
-
-    /// Run all tasks in the pool to completion.
-    ///
-    /// ```
-    /// use futures::executor::LocalPool;
-    ///
-    /// let mut pool = LocalPool::new();
-    ///
-    /// // ... spawn some initial tasks using `spawn.spawn()` or `spawn.spawn_local()`
-    ///
-    /// // run *all* tasks in the pool to completion, including any newly-spawned ones.
-    /// pool.run();
-    /// ```
-    ///
-    /// The function will block the calling thread until *all* tasks in the pool
-    /// are complete, including any spawned while running existing tasks.
-    pub fn run(&mut self, parameter: &mut T) {
-        // hoist the T:
-        unsafe {
-            *self.data = Some(NonNull::new_unchecked(parameter as *mut _));
-        }
-
-        run_executor(|cx| self.poll_pool(cx));
-
-        // for segfault help, we null here
-        *self.data = None;
-    }
-
-    /// Runs all the tasks in the pool until the given future completes.
-    ///
-    /// ```
-    /// use futures::executor::LocalPool;
-    ///
-    /// let mut pool = LocalPool::new();
-    /// # let my_app  = async {};
-    ///
-    /// // run tasks in the pool until `my_app` completes
-    /// pool.run_until(my_app);
-    /// ```
-    ///
-    /// The function will block the calling thread *only* until the future `f`
-    /// completes; there may still be incomplete tasks in the pool, which will
-    /// be inert after the call completes, but can continue with further use of
-    /// one of the pool's run or poll methods. While the function is running,
-    /// however, all tasks in the pool will try to make progress.
-    // pub fn run_until<F: Future>(&mut self, future: F) -> F::Output {
-    //     pin_mut!(future);
-
-    //     run_executor(|cx| {
-    //         {
-    //             // if our main task is done, so are we
-    //             let result = future.as_mut().poll(cx);
-    //             if let Poll::Ready(output) = result {
-    //                 return Poll::Ready(output);
-    //             }
-    //         }
-
-    //         let _ = self.poll_pool(cx);
-    //         Poll::Pending
-    //     })
-    // }
-
-    /// Runs all tasks and returns after completing one future or until no more progress
-    /// can be made. Returns `true` if one future was completed, `false` otherwise.
-    ///
-    /// ```
-    /// use futures::{
-    ///     executor::LocalPool,
-    ///     future::{pending, ready},
-    ///     task::LocalSpawnExt,
-    /// };
-    ///
-    /// let mut pool = LocalPool::new();
-    /// let spawner = pool.spawner();
-    ///
-    /// spawner.spawn_local(ready(())).unwrap();
-    /// spawner.spawn_local(ready(())).unwrap();
-    /// spawner.spawn_local(pending()).unwrap();
-    ///
-    /// // Run the two ready tasks and return true for them.
-    /// pool.try_run_one(); // returns true after completing one of the ready futures
-    /// pool.try_run_one(); // returns true after completing the other ready future
-    ///
-    /// // the remaining task can not be completed
-    /// assert!(!pool.try_run_one()); // returns false
-    /// ```
-    ///
-    /// This function will not block the calling thread and will return the moment
-    /// that there are no tasks left for which progress can be made or after exactly one
-    /// task was completed; Remaining incomplete tasks in the pool can continue with
-    /// further use of one of the pool's run or poll methods.
-    /// Though only one task will be completed, progress may be made on multiple tasks.
-    // pub fn try_run_one(&mut self) -> bool {
-    //     poll_executor(|ctx| {
-    //         loop {
-    //             let ret = self.poll_pool_once(ctx);
-
-    //             // return if we have executed a future
-    //             if let Poll::Ready(Some(_)) = ret {
-    //                 return true;
-    //             }
-
-    //             // if there are no new incoming futures
-    //             // then there is no feature that can make progress
-    //             // and we can return without having completed a single future
-    //             if self.incoming.borrow().is_empty() {
-    //                 return false;
-    //             }
-    //         }
-    //     })
-    // }
-
-    /// Runs all tasks in the pool and returns if no more progress can be made
-    /// on any task.
-    ///
-    /// ```
-    /// use futures::{
-    ///     executor::LocalPool,
-    ///     future::{pending, ready},
-    ///     task::LocalSpawnExt,
-    /// };
-    ///
-    /// let mut pool = LocalPool::new();
-    /// let spawner = pool.spawner();
-    ///
-    /// spawner.spawn_local(ready(())).unwrap();
-    /// spawner.spawn_local(ready(())).unwrap();
-    /// spawner.spawn_local(pending()).unwrap();
-    ///
-    /// // Runs the two ready task and returns.
-    /// // The empty task remains in the pool.
-    /// pool.run_until_stalled();
-    /// ```
-    ///
-    /// This function will not block the calling thread and will return the moment
-    /// that there are no tasks left for which progress can be made;
-    /// remaining incomplete tasks in the pool can continue with further use of one
-    /// of the pool's run or poll methods. While the function is running, all tasks
-    /// in the pool will try to make progress.
-    pub fn run_until_stalled(&mut self, parameter: &mut T) {
-        // hoist the T:
-        unsafe {
-            *self.data = Some(NonNull::new_unchecked(parameter as *mut _));
-        }
-
-        poll_executor(|ctx| {
-            let _ = self.poll_pool(ctx);
-        });
-
-        // null it
-        *self.data = None;
-    }
-
-    // Make maximal progress on the entire pool of spawned task, returning `Ready`
-    // if the pool is empty and `Pending` if no further progress can be made.
-    fn poll_pool(&mut self, cx: &mut Context<'_>) -> Poll<()> {
-        // state for the FuturesUnordered, which will never be used
-        loop {
-            let ret = self.poll_pool_once(cx);
-
-            // we queued up some new tasks; add them and poll again
-            // if !self.incoming.is_empty() {
-            //     continue;
-            // }
-
-            // no queued tasks; we may be done
-            match ret {
-                Poll::Pending => return Poll::Pending,
-                Poll::Ready(None) => return Poll::Ready(()),
-                _ => {}
-            }
-        }
-    }
-
-    // Try make minimal progress on the pool of spawned tasks
-    fn poll_pool_once(&mut self, cx: &mut Context<'_>) -> Poll<Option<()>> {
-        // grab our next task...
-        if self.pool.is_empty() {
-            if let Some(task) = self.incoming.lock().unwrap().pop_front() {
-                unsafe { self.pool.push(task.into_future_object()) }
-            }
-        }
-
-        // try to execute the next ready future
-        Pin::new(&mut self.pool).poll_next(cx)
-    }
-}
-
-impl<T: 'static> Default for Cosync<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-// /// Turn a stream into a blocking iterator.
-// ///
-// /// When `next` is called on the resulting `BlockingStream`, the caller
-// /// will be blocked until the next element of the `Stream` becomes available.
-// pub fn block_on_stream<S: Stream + Unpin>(stream: S) -> BlockingStream<S> {
-//     BlockingStream { stream }
-// }
-
-// /// An iterator which blocks on values from a stream until they become available.
-// #[derive(Debug)]
-// pub struct BlockingStream<S: Stream + Unpin> {
-//     stream: S,
-// }
-
-// impl<S: Stream + Unpin> Deref for BlockingStream<S> {
-//     type Target = S;
-//     fn deref(&self) -> &Self::Target {
-//         &self.stream
-//     }
-// }
-
-// impl<S: Stream + Unpin> DerefMut for BlockingStream<S> {
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         &mut self.stream
-//     }
-// }
-
-// impl<S: Stream + Unpin> BlockingStream<S> {
-//     /// Convert this `BlockingStream` into the inner `Stream` type.
-//     pub fn into_inner(self) -> S {
-//         self.stream
-//     }
-// }
-
-// impl<S: Stream + Unpin> Iterator for BlockingStream<S> {
-//     type Item = S::Item;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         LocalPool::new().run_until(self.stream.next())
-//     }
-
-//     fn size_hint(&self) -> (usize, Option<usize>) {
-//         self.stream.size_hint()
-//     }
-// }
-
-// impl Spawn for LocalSpawner {
-//     fn spawn_obj(&self, future: FutureObj<'static, ()>) -> Result<(), SpawnError> {
-//         if let Some(incoming) = self.incoming.upgrade() {
-//             incoming.borrow_mut().push(future.into());
-//             Ok(())
-//         } else {
-//             Err(SpawnError::shutdown())
-//         }
-//     }
-
-//     fn status(&self) -> Result<(), SpawnError> {
-//         if self.incoming.upgrade().is_some() {
-//             Ok(())
-//         } else {
-//             Err(SpawnError::shutdown())
-//         }
-//     }
-// }
-
-// impl LocalSpawn for LocalSpawner {
-//     fn spawn_local_obj(&self, future: LocalFutureObj<'static, ()>) -> Result<(), SpawnError> {
-//         if let Some(incoming) = self.incoming.upgrade() {
-//             incoming.borrow_mut().push(future);
-//             Ok(())
-//         } else {
-//             Err(SpawnError::shutdown())
-//         }
-//     }
-
-//     fn status_local(&self) -> Result<(), SpawnError> {
-//         if self.incoming.upgrade().is_some() {
-//             Ok(())
-//         } else {
-//             Err(SpawnError::shutdown())
-//         }
-//     }
-// }
-
 /// Sleep for a given number of ticks.
 pub fn sleep_ticks(ticks: usize) -> SleepForTick {
     SleepForTick::new(ticks)
 }
 
-/// A helper struct which registers a sleep for a given number
-/// of ticks.
+/// A helper struct which registers a sleep for a given number of ticks.
 #[derive(Clone, Copy, Debug)]
-pub struct SleepForTick(usize);
+pub struct SleepForTick(pub usize);
 
 impl SleepForTick {
     /// Sleep for the number of ticks provided.
@@ -573,12 +400,12 @@ mod tests {
         executor.queue(move |mut input| async move {
             let mut input = input.get();
 
-            assert_eq!(**input, 10);
-            **input = 10;
+            assert_eq!(*input, 10);
+            *input = 10;
         });
 
         executor.queue(move |mut input| async move {
-            assert_eq!(**input.get(), 10);
+            assert_eq!(*input.get(), 10);
 
             // this will make the executor sleep, stall,
             // and exit out of this tick
@@ -587,7 +414,7 @@ mod tests {
             let sleep = SleepForTick(1);
             sleep.await;
 
-            let input = &mut **input.get();
+            let input = &mut *input.get();
             assert_eq!(*input, 30);
             *input = 0;
         });
@@ -609,15 +436,15 @@ mod tests {
         let mut executor: Cosync<i32> = Cosync::new();
         executor.queue(move |mut input| async move {
             println!("starting task 1");
-            **input.get() = 10;
+            *input.get() = 10;
 
             sleep_ticks(100).await;
 
-            **input.get() = 20;
+            *input.get() = 20;
         });
 
         executor.queue(move |mut input| async move {
-            assert_eq!(**input.get(), 20);
+            assert_eq!(*input.get(), 20);
         });
 
         value = 0;
@@ -632,19 +459,19 @@ mod tests {
         let mut executor: Cosync<i32> = Cosync::new();
         executor.queue(move |mut input| async move {
             println!("starting task 1");
-            **input.get() = 10;
+            *input.get() = 10;
 
             input.queue(move |mut input| async move {
                 println!("starting task 3");
-                assert_eq!(**input.get(), 20);
+                assert_eq!(*input.get(), 20);
 
-                **input.get() = 30;
+                *input.get() = 30;
             });
         });
 
         executor.queue(move |mut input| async move {
             println!("starting task 2");
-            **input.get() = 20;
+            *input.get() = 20;
         });
 
         // initialized here, after tasks are made
