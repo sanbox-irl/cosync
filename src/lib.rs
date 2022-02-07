7738c93ce3a9dffb0c6369e618cfd6e2e968a814
Jonathan Spira <jjspira@gmail.com>
2022-02-06 23:19:11 -0500

    updated lib for release

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,45 @@
+//! This crate provides a singlethreaded, sequential, parameterized async runtime.
+//! In other words, this creates *coroutines*, specifically targetting video game logic,
+//! though it's suitable for creating any sequences of directions which take time.
+//!
+//! Here's a basic `Cosync` example:
+//!
+//! ```
+//! # use cosync::Cosync;
+//! // the type parameter is the *value* which other functions will get.
+//! let mut Cosync: Cosync<i32> = Cosync::new();
+//!
+//! let example_move = 20;
+//!
+//! // there are a few ways to queue tasks, but here's a simple one:
+//! cosync.queue(move |input: CosyncInput<i32>| async move {
+//!     // set our input to `example_move`...
+//!     *input.get() = example_move;
+//! });
+//!
+//! let mut value = 0;
+//! cosync.run_until_stall(&mut value);
+//! assert_eq!(value, example_move);
+//! ```
+//!
+//! This crate exposes two methods for driving the runtime: `run_until_stall`
+//! and `run_blocking`. You generally want `run_until_stall`, which attempts to do
+//! the task given, until it cannot (ie, is waiting), at which point is returns
+//! control to the caller.
+//!
+//! Finally, this crate supports sending tasks from other threads. There are three ways
+//! to make new tasks. First, the `Cosync` struct itself has a `queue` method on it. Secondly,
+//! each task gets a `CosyncInput<T>` as a parameter, which has `get` (to get access to your `&mut
+//! T`) and `queue` to queue another task (which is at the end of the queue, not necessarily after
+//! the task which added it). Lastly, you can create a `CosyncQueueHandle` with
+//! `Cosync::create_queue_handle` which is `Send` and can be given to other threads to create new
+//! tasks for the `Cosync`.
+//!
+//! This crate depends on only `std`. It is in an early state of development, but is in
+//! production ready state right now
+
 #![deny(rust_2018_idioms)]
+#![deny(missing_docs)]
 
 // this is vendored code from the `futures-rs` crate, to avoid
 // having a huge dependency when we only need a little bit
@@ -199,7 +240,7 @@ impl<T: 'static> Cosync<T> {
         queue_handle.queue_task(task)
     }
 
-    /// Run all tasks in the pool to completion.
+    /// Run all tasks in the queue to completion. You probably want `run_until_stall`.
     ///
     /// ```
     /// # use cosync::Cosync;
